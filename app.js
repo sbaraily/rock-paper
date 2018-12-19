922c82b4941ab063f7878f7582d2c3de020ad6f3
Samir Baraily <samir.baraily@gmail.com>
2018-12-18 23:29:29 -0700

    added codes

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 
-var choices = ['rock', 'paper','scissors'];
+var choices = ['Rock', 'Paper','Scissors'];
 var systemChooses = choices[Math.floor(Math.random()*choices.length)];
+
+var btn1 = document.getElementById("btn1")
+var btn2 = document.getElementById("btn2")
+var btn3 = document.getElementById("btn3")
+
+btn1.addEventListener("click", choiceOne)
+btn2.addEventListener("click", choiceTwo)
+btn3.addEventListener("click", choiceThree)
+
+
+function choiceOne(){
+  return choices[0];
+}
+function choiceTwo(){
+  return choices[1];
+}
+function choiceThree(){
+  return choices[2];
+}
+
